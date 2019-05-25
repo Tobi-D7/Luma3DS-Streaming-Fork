41a909e1d12c035420dda5f1f3c1092daa6af963
byebyesky <Byebyesky@users.noreply.github.com>
2019-05-25 10:58:05 +0200

    Added new3ds check

--- sysmodules/rosalina/include/menus/streaming.h
+++ sysmodules/rosalina/include/menus/streaming.h
@@ -11,7 +11,3 @@ void endThread(void);
 void finalize();
 void rpCloseGameHandle();
 void closeRPHandle();
-//int acceptClient(struct sock_ctx *ctx);
-//int closeConnection(struct sock_ctx *ctx);
-//struct sock_ctx *allocClient(struct sock_server *server, u16 port);
-//void releaseClient(struct sock_server *server, struct sock_ctx *ctx);
